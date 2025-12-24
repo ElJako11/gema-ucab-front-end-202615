3c321359768ca52f41b82bcb39f5418aa5372a25
carlos19men <camendez.23@est.ucab.edu.ve>
2025-12-24 07:48:35 -0400

    fix: cambié el titulo, grupos de trabajo por área

--- app/(dashboard)/tecnicos/page.tsx
+++ app/(dashboard)/tecnicos/page.tsx
@@ -124,7 +124,7 @@ const Tecnicos = () => {
                 Correo
               </th>
               <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase">
-                Grupos de Trabajo
+                Area
               </th>
             </tr>
           </thead>
